b181abdd40fb35f31b806115f235c5f3cfca85b7
Mark Celoza <markangeloceloza380@gmail.com>
2024-01-10 14:19:53 +0800

    Input Rootlayout File

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import RootLayout from "./RootLayout";
+
 function App() {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route element={<RootLayout />}>
+          <Route path="/" element={<Home />} />
+        </Route>
       </Routes>
     </div>
   );
